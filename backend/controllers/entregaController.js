a74e4ec75c11eff4e724be3318b52a7546bce533
Cristian Soto <crisandru1997@gmail.com>
2023-09-01 01:28:30 -0400

    Nuevas mejoras

--- backend/controllers/entregaController.js
+++ backend/controllers/entregaController.js
@@ -2,43 +2,37 @@ const entrega=require('../models/entrega');
 
 const createEntrega =(req,res) =>{
     const {tipo, marca, modelo, n_serie, cod_servicio, imei, pin, puk, fecha_compra, descripcion, procesador, ram, tipo_almacenamiento, capacidad, observacion, otorga, recibe}=req.body;
-
-    if(tipo && marca && modelo && n_serie && cod_servicio && imei && pin && puk && fecha_compra && descripcion && procesador && ram && tipo_almacenamiento && capacidad && observacion && otorga && recibe){
-        const newEntrega = new entrega({
-            tipo,
-            marca,
-            modelo,
-            n_serie,
-            cod_servicio,
-            imei,
-            pin,
-            puk,
-            fecha_compra,
-            descripcion,
-            procesador,
-            ram,
-            tipo_almacenamiento,
-            capacidad,
-            observacion,
-            otorga,
-            recibe
-        });
-
-        try {
-            newEntrega.save();
-
-            res.status(202).json({
-                message: "entrega realizada correctamente"
-            })
-        }
-        catch (error) {
-            res.status(400).json({
-                message: 'No se puede registrar la entrega'
-            })
-        }
-    }else{
-        return res.status(400).json({
-            mensaje:"Por favor llene todos los campos"
+    console.log(req.body);
+    const newEntrega = new entrega({
+        tipo,
+        marca,
+        modelo,
+        n_serie,
+        cod_servicio,
+        imei,
+        pin,
+        puk,
+        fecha_compra,
+        descripcion,
+        procesador,
+        ram,
+        tipo_almacenamiento,
+        capacidad,
+        observacion,
+        otorga,
+        recibe
+    });
+    try {
+        newEntrega.save();
+        console.log("entrega registrada correctamente");
+        res.status(202).json({
+            message: "entrega realizada correctamente"
+        })
+    }
+    catch (error) {
+        console.log("No se puede registrar la entrega: ", error);
+        res.status(400).json({
+            message: 'No se puede registrar la entrega'
         })
     }
 }
@@ -65,7 +59,7 @@ const getEntrega = (req,res)=>{
 
 const updateEntrega = (req,res)=>{
     const {id} = req.params;
-
+    
     entrega.findByIdAndUpdate(id,req.body,(err,entrega)=>{
         if (!entrega) {
             return res.status(404).send({ message: "No se ha encontrado la entrega" })
